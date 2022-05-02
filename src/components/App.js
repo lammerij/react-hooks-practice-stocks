97d7879ae096c103f75ff34e63f6c742d94baa8a
+ <+>
2022-05-02 19:29:22 -0400

    commit tree ef0a53b982224b9d555efd205a0b8fbdab08371b

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ function App() {
           return {...stk, bought: false}
         })
         setStocks(newData)
+
       })
   }, []);
 
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,7 +6,8 @@ function PortfolioContainer({portfolio}) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {portfolio.map((stock) => (
+      {portfolio.map((stock) =>  (
+        
         <Stock stock={stock} key={stock.id} />
       ))}
     </div>
